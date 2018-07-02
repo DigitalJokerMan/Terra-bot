1f31db91009d7a914f43d900a07eb319aa9f4451
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-02 14:08:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -128,10 +128,10 @@ function handleCommand(message, command, args) {
 		message.channel.send(`ID:  ${message.author.id}`);
 	}
 	if (command == "dice") {
-		let rolls = args[0];
-		let min = args[1];
-		let max = args[2];
-		for(var i = 0; i > rolls; i++) {
+		let rolls = args[0] || 1;
+		let min = args[1] || 1;
+		let max = args[2] || 6;
+		for(var i = 0; i < rolls; i++) {
 			message.channel.send(Math.floor(Math.random()*(max-min+1)+min));
 		}
 	}
