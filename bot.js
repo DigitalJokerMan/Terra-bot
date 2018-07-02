ee9051d90f171d799b121073661d77332f2fd4f7
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-02 14:12:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -128,12 +128,15 @@ function handleCommand(message, command, args) {
 		message.channel.send(`ID:  ${message.author.id}`);
 	}
 	if (command == "dice") {
+		let message = "";
 		let rolls = args[0] || 1;
 		let min = args[1] || 1;
 		let max = args[2] || 6;
 		for(var i = 0; i < rolls; i++) {
-			message.channel.send(Math.floor(Math.random()*(max-min+1)+min));
+			let dice = Math.floor(Math.random()*(max-min+1)+min);
+			message = message.concat(" ").concat(dice);
 		}
+		message.channel.send(message);
 	}
 	if (command == "uptime") {
 			const embed = new Discord.RichEmbed()
