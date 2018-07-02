5a422b29c197dc54767ea50bc3d1ca84cf268562
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-03 01:05:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,9 +90,9 @@ function handleCommand(message, command, args) {
 				 let resultname;
 			    search(args.join(' '), opts, function(err, results) {
  				 if(err) return console.log(err);
-			    });
 				 result = results[0].link;
 				 resultname = results[0].title;
+			    });
 				 console.log(result);
 				 console.log(resultname);
  			     const connection = message.member.voiceChannel.join().then(connection => {
