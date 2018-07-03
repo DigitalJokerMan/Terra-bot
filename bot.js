9804b15a21b83a26b45bcb9ca0d4527cc1749a45
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-03 16:07:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -172,6 +172,21 @@ function handleCommand(message, command, args) {
 			});
     			message.channel.send(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
  	}
+	if (command == "ban") {
+			let caller = message.guild.members.get(message.author.id);
+			let has_ban = caller.hasPermission("BAN_MEMBERS");
+    			if(!has_ban) return message.reply("Sorry, you don't have permissions to use this!");
+    			let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    			if(!member) return message.reply("Please mention a valid member of this server");
+    			if(!member.banable) return message.reply("I cannot ban this user");
+    				let reason = message.content.replace(";kick ", "");
+				reason = reason.replace(member, "");
+    			if(!reason) reason = "No reason provided";
+			member.send(`You have been banned from ${message.guild.name} by ${message.author.tag} for reason: ${reason}`).then(kicke => {
+    			member.ban(reason)
+			});
+    			message.channel.send(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+ 	}
 	if (command == "help") {
 		const embed = new Discord.RichEmbed()
 		.setColor(530118)
