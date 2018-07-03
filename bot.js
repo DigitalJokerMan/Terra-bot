6bbf1137f49ca69e0a629899c63dc34bb296636c
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-03 19:19:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -109,9 +109,9 @@ function handleCommand(message, command, args) {
 				 resultname = results[0].title;
 				servers[message.guild.id].queue.push(result);
 				servers[message.guild.id].songName.push(resultname);
+			    })
 				 console.log(servers[message.guild.id].queue);
 			    	console.log(servers[message.guild.id].songName);
-			    })
  			     const connection = message.member.voiceChannel.join().then(connection => {
 			     message.reply(`Added ${servers[message.channel.guild.id].songname[0]} to the queue`);
             			const stream = ytdl(servers[message.channel.guild.id].queue[0],  { filter : 'audioonly' });
