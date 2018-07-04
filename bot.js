06f33ddfaed325975c14db0ed87df1133ee19f3c
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-04 15:55:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ var opts = {
 };
 client.on('ready', () => {
   console.log(`Bots is ready and working in ${client.guilds.size} servers with ${client.users.size} users!`);
-  client.user.setActivity("Terradice&RedSponge|(help");
+  client.user.setActivity("Terradice&RedSponge|;help");
 	/*for(var id of client.guilds.keys()) {
 		console.log(id);
 		servers["" + id]["queue"] = [];
