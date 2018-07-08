77ea9c9ea4e730adebe8cfd706ce049285ba0523
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-08 12:43:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -76,7 +76,7 @@ function handleCommand(message, command, args) {
 	if(command == "eval") {
 		if(message.author.id !== "244111430956089344" && message.author.id !== "263995600641589248") return;
 		let pidor = args.join(" ");
-		if (pidor.includes("process.env.TOKEN") || pidor.includes("client.token")) {
+		if (eval(pidor) == client.token) {
 			message.reply("Nice try");
 			return;
 		}
