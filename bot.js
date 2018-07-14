13175dd252297abef942327109904d178cdc5564
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-14 22:08:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -92,6 +92,8 @@ async function handleCommand(message, command, args) {
 		message.reply("Click the link below to add me to your server https://discordapp.com/oauth2/authorize?client_id=459782347936628747&scope=bot&permissions=8");
 	}
 	if (command == "urban") {
+		urban('hello').then(console.log);
+		return;
 		let search = args.join(" ");
 		let result = urban(search);
 		let title = result.word;
