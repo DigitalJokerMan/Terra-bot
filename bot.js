63250aaf649db971ae2da44eb390ced8f65b8b9a
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-14 22:55:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -103,9 +103,7 @@ async function handleCommand(message, command, args) {
 			let tags = result.tags.join(" ");
 			const embed = new Discord.RichEmbed()
 			.setColor(color)
-			.setFooter(`Terrabot operating in ${client.guilds.size} servers`)
-			.setAuthor("Terrabot", client.user.avatarURL)
-			.setTitle(title).setURL(link)
+			.setAuthor(title).setURL(link)
 			.addField("Definition", description , false)
 			.addField("examples", example, false)
 			.addField("tags", tags, false)
