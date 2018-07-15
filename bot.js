17895e0f8e56b0f3d7fdd1198116bf5890a256d0
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-15 22:30:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -123,7 +123,7 @@ async function handleCommand(message, command, args) {
 	if (command == "danbooru") {
 		booru.posts({ tags: "rating:explicit:favcount:>50"}).then(async (posts) => {
 			  const index = Math.floor(Math.random() * posts.length);
- 			  const posts = await booru.posts({ limit: 200, tags: args.join("_") })
+ 			  posts = await booru.posts({ limit: 200, tags: args.join("_") })
 			const url = booru.url(post.file_url);
   			const name = `${post.md5}`;
 			//{files: [link]}
