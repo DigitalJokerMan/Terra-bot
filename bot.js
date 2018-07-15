f621be4cc874e7778a6476b6e567fa1b25145d23
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-16 02:14:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -125,7 +125,7 @@ async function handleCommand(message, command, args) {
 		booru.posts({ tags: 'rating:e order:favcount limit:200' }).then(async (posts) => {
 			  const index = Math.floor(Math.random() * posts.length)
 			  const post = await posts[index]
-			  if(!post) return console.log('error');
+			  if(!post) console.log('error');
   			const name = `${post.md5}.${post.file_ext}`
 			const embed = new Discord.RichEmbed()
 			.setColor(color)
