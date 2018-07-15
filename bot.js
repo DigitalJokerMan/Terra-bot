c71e8578b7e6e44046d12972db6750f36a0030ff
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-15 20:59:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -121,7 +121,7 @@ async function handleCommand(message, command, args) {
 		message.reply("Click the link below to add me to your server https://discordapp.com/oauth2/authorize?client_id=459782347936628747&scope=bot&permissions=8");
 	}
 	if (command == "danbooru") {
-		booru.posts({ tags: args.join(" "); }).then(posts => {
+		booru.posts({ tags: args.join(" ") }).then(posts => {
 			  const index = Math.floor(Math.random() * posts.length)
  			  const post = posts[index]
 			  
