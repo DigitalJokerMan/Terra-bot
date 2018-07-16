c8503a9c2657fae464c90098c1063ffe2a6a62ee
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-16 04:28:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -127,7 +127,6 @@ async function handleCommand(message, command, args) {
 		}
 		message.channel.send("<a:googling:426453223310622740>" + " Loading...").then(msg => {
 			booru.posts({ tags: 'rating:e order:favcount limit:200' }).then(async (posts) => {
-				console.log(posts);
 				  const index = Math.floor(Math.random() * posts.length);
 				  const post = await posts[index];
 				  if(!post) {
