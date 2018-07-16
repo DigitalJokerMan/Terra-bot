b3f3ae714599fc7e426b984dede1646efd868d61
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-16 17:54:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -131,7 +131,7 @@ async function handleCommand(message, command, args) {
 		message.channel.send("<a:googling:426453223310622740>" + " Loading...").then(msg => {
 			booru.posts({ tags: 'rating:e order:favcount limit:200' }).then(posts => {
 				console.log(posts instanceof);
-				if (posts instanceof Array) {
+				if (posts typeof) {
 					  console.log('posts is an array')
 					} else {
  				 console.log(JSON.stringify(posts))
