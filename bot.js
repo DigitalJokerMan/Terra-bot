a1775aad81b7b4e649784dcfdeaec12cbfcddaf4
Terradice <35463279+Terradice@users.noreply.github.com>
2018-07-19 11:17:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');const Discord = require('discord.js');
+const Discord = require('discord.js');
 const google = require('google');
 const config = require("./config.json");
 const Danbooru = require('danbooru')
